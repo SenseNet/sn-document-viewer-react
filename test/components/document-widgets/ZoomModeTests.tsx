db82795840a8689f28754acbc2caeccedf209eed
gallayl <gallay.lajos@gmail.com>
2018-04-06 09:23:18 +0200

    [KFI]test(ComponentTests): added test for zoom mode doc.widget (partial)

--- test/components/document-widgets/ZoomModeTests.tsx
+++ test/components/document-widgets/ZoomModeTests.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 
+import { expect } from 'chai'
+import { IconButton, Menu, MenuItem } from 'material-ui'
 import * as renderer from 'react-test-renderer'
 import { ZoomModeWidget } from '../../../src/components/document-widgets/ZoomMode'
 import { documentReceivedAction } from '../../../src/store/Document'
@@ -24,5 +26,38 @@ export const zoomModeWidgetTests = describe('ZoomModeWidget component', () => {
                     </ZoomModeWidget>
                 </Provider>)
         })
+    })
+
+    it('ZoomMenu open / close', () => {
+        useTestContext((ctx) => {
+            ctx.store.dispatch(documentReceivedAction(exampleDocumentData))
+
+            c = renderer.create(
+                <Provider store={ctx.store}>
+                    <ZoomModeWidget>
+                    </ZoomModeWidget>
+                </Provider>)
+            const zoomMenuButton = c.root.findByType(IconButton)
+            zoomMenuButton.props.onClick({currentTarget: undefined})
+            const zoomMenu = c.root.findByType(Menu)
+            zoomMenu.props.onClose()
+        })
+    })
+
+    it('set zoom mode to fit', () => {
+        // useTestContext((ctx) => {
+        //     ctx.store.dispatch(documentReceivedAction(exampleDocumentData))
+
+        //     c = renderer.create(
+        //         <Provider store={ctx.store}>
+        //             <ZoomModeWidget>
+        //             </ZoomModeWidget>
+        //         </Provider>)
+        //     const zoomMenuButton = c.root.findByType(IconButton)
+        //     zoomMenuButton.props.onClick({currentTarget: undefined})
+        //     const button = c.root.findAllByType(MenuItem)[0];
+        //     (button as any).props.onClick({})
+        //     expect(ctx.store.getState().sensenetDocumentViewer.viewer.zoomMode).to.be.eq('fit')
+        // })
     })
 })
