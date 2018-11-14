4a0f55f3d31d9cedb8c2615d94e2a59de7aaf5af
Zoltán Bedi <zoltan.bedi@gmail.com>
2018-11-14 13:48:05 +0100

    refactor(TypeHelper): add asNumber function
    With asNumber you can check if a value is a number or null

--- src/services/TypeHelpers.ts
+++ src/services/TypeHelpers.ts
@@ -12,13 +12,28 @@ import { RootReducerType } from '../store'
  * ```
  */
 export type componentType<
-    TStateToProps extends MapStateToProps<TState, TOwnProps, State>,
-    TDispatchToProps,
-    TOwnProps = undefined,
-    TState = ReturnType<TStateToProps>,
-    State = RootReducerType,
-    > = (
-        ReturnType<TStateToProps> &
-        TDispatchToProps &
-        TOwnProps
-    )
+  TStateToProps extends MapStateToProps<TState, TOwnProps, State>,
+  TDispatchToProps,
+  TOwnProps = undefined,
+  TState = ReturnType<TStateToProps>,
+  State = RootReducerType
+> = ReturnType<TStateToProps> & TDispatchToProps & TOwnProps
+
+/**
+ * Casts an arbitrary value to a number. Both null and undefined are treated as undefined values by default.
+ * @param value An arbitrary value to convert to a number.
+ * @param defaultValue The default value that is used when the specified value is either null or undefined values.
+ * @returns Number | NaN | undefined
+ * @example
+ *    undefined = asNumber(undefined)
+ *    undefined = asNumber(null)
+ *    10        = asNumber(10)
+ *    10        = asNumber('10')
+ *    10        = asNumber({ toString() { return '10'; } })
+ *    NaN       = asNumber('Hello')
+ * @see https://github.com/winjs/winjs/blob/e0ad943d1cd597b2deeddfdb04ab9462c3ab7ce7/src/js/WinJS/BindingList.js#L38
+ */
+export function asNumber(value: any, defaultValue?: number): number | undefined {
+  const num = value === null || value === undefined || value === '' ? defaultValue : +value
+  return num
+}
